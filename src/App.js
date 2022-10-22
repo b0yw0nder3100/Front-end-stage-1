46e4c512efd0d1fb382f219fe8c94c6a35c836a9
Martins Ikpe <martins.ikpe@stu.cu.edu.ng>
2022-10-22 22:01:53 +0100

    Added Header Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import styled from 'styled-components'
+
+import Header from './components/Header'
+
 
 const App = () => {
 
   return (
     <div>
+      <Header></Header>
 
     </div>
   )
