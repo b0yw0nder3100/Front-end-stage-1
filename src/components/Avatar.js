8f89c93a3c81ca7c2d129180ccc1b303c35e2e8e
Martins Ikpe <martins.ikpe@stu.cu.edu.ng>
2022-10-22 21:13:46 +0100

    added avatar component

---
+++ src/components/Avatar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import styled from 'styled-components'
+/**
+ * Component for showing details of the avatar
+ * @component
+ * @param {image} picture - Profile Picture of the User
+ * @param {string} twitter_name - Twitter username of the User
+ * @param {string} slack_name - Slack name of the User
+ */
+const Avatar = ({ picture, twitter_name, slack_name }) => {
+
+    return (
+        <div>
+            <img src={picture} alt="profile" id="profile__img"></img>
+            <Twitter id="twitter">{twitter_name}</Twitter>
+            <Slack id="slack">{slack_name}</Slack>
+        </div>
+    )
+}
+
+export default Avatar
+
+const Twitter = styled.p`
+    padding: 24px 0 0 0
+`
+const Slack = styled.p`
+    display:none
+`
---
+++ src/data/profile.js
@@ -0,0 +1,20 @@
+import profilePicture from '../assets/media/profile__img.svg'
+/**
+ * Profile
+ * @typedef {object} Profile
+ * @property {image} im - Profile Picture
+ * @property {string} twitter_name - Twitter Username
+ * @property {string} slack_name - Slack Name
+ */
+
+/**
+ * @type {Profile}
+ */
+const Profile = {
+    img: profilePicture,
+    twitter_name: "Martins_Ikpe",
+    slack_name: "Martins Ikpe"
+}
+
+
+export default Profile;
