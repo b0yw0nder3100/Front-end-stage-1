37033aca62be1581918555f26a79cc449683d862
Martins Ikpe <martins.ikpe@stu.cu.edu.ng>
2022-10-31 14:09:13 +0100

    Fix: Profile image not aligned to center

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -9,24 +9,35 @@ import styled from 'styled-components'
  */
 const Avatar = ({ picture, twitter_name, slack_name, slack_ID }) => {
     return (
-        <div>
-            <img src={picture} alt="profile" id="profile__img"></img>
+        <Div>
+            <Img src={picture} alt="profile" id="profile__img"></Img>
             <Twitter id="twitter">@{twitter_name}</Twitter>
             <Slack id="slack">
                 Member ID:{slack_ID}
                 Display Name:{slack_name}</Slack>
-        </div>
+        </Div>
     )
 }
 
 export default Avatar
 
+const Img = styled.img`
+width: 88px;
+height: 88px;
+display:flex;
+align-self:center;
+`
+
 const Twitter = styled.p`
-    padding: 24px 0 0 0;
-    font-weight: 700;
-    font-size: 20px;
-    line-height: 30px;
+padding: 24px 0 0 0;
+font-weight: 700;
+font-size: 20px;
+line-height: 30px;
 `
 const Slack = styled.p`
     display:none;
 `
+const Div = styled.div`
+display:flex;
+justify-content: center;
+flex-direction:column;`
