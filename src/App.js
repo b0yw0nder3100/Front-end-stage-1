b4b928b66a960ec5b2e16b617d40cfb0eb805078
Martins Ikpe <martins.ikpe@stu.cu.edu.ng>
2022-11-03 14:22:44 +0100

    feat: Updated Route for contact me

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Routes, Route } from 'react-router'
+import Contactpage from './pages/Contactpage'
 import Homepage from './pages/Homepage'
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <div>
       <Routes>
         <Route path='/' element={<Homepage></Homepage>}></Route>
+        <Route path='/contact' element={<Contactpage></Contactpage>}></Route>
       </Routes>
     </div>
   )
